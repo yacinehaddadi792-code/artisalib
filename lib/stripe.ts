c51e26b266584e26fce3efc082d50cb06ac6686c
Yacine Haddadi <yacinehaddadi792@gmail.com>
2026-04-03 01:16:02 +0200

    fix stripe api version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder', {
-  apiVersion: '2024-10-28.acacia',
+  apiVersion: '2024-11-20.acacia',
 });
 
 export const STRIPE_PRICE_MAP = {
