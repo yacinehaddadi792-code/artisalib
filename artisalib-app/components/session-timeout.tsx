ab90d1efa31a711d54cffabff3dbb0c337fdb972
Yacine Haddadi <yacinehaddadi792@gmail.com>
2026-05-03 14:36:07 +0200

    fix varaible

--- artisalib-app/components/session-timeout.tsx
+++ artisalib-app/components/session-timeout.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 export default function SessionTimeout() {
   useEffect(() => {
-    let timer: NodeJS.Timeout;
+    let timer: ReturnType<typeof setTimeout>;
 
     const resetTimer = () => {
       clearTimeout(timer);
